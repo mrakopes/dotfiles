290c1184a0a2e06eade6d31eab0ca1f3c606121b
David Ciganek <david.ciganek@wedos.com>
2020-05-18 08:39:38 +0200

    upd: ~/.config/awesome/rc.lua swap layout positions

--- home/.config/awesome/rc.lua
+++ home/.config/awesome/rc.lua
@@ -65,8 +65,8 @@ modkey = "Mod4"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
-    awful.layout.suit.tile,
     awful.layout.suit.tile.top,
+    awful.layout.suit.tile,
     awful.layout.suit.floating,
     awful.layout.suit.spiral,
     -- awful.layout.suit.spiral.dwindle,
